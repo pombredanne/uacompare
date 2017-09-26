ff32c7d434793cc55f2ef98b1dd30ee1b19187fe
Davor Kapsa <davor.kapsa@gmail.com>
2017-09-26 20:50:51 +0200

    add TestLoadRawData

--- main_test.go
+++ main_test.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestLoadRawData(t *testing.T) {
+	const path = "testdata/testdata.txt"
+	rd, err := loadRawData(path)
+	if err != nil {
+		t.Errorf("err: %v", err)
+	}
+	if len(rd) == 0 {
+		t.Error("empty data")
+	}
+}
