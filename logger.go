28815e37fa5a422a5ebe5a6d97cc47238a6054aa
Davor Kapsa <davor.kapsa@gmail.com>
2017-09-27 16:35:15 +0200

    add newLogger

--- logger.go
+++ logger.go
@@ -1,10 +1,36 @@
 package main
 
-import "log"
+import (
+	"io"
+	"log"
+	"os"
+)
 
 // Logger holds all log levels.
 type Logger struct {
 	il *log.Logger
 	dl *log.Logger
 	el *log.Logger
+}
+
+func newLogger(debug bool, stdout, stderr io.Writer) *Logger {
+
+	if stdout == nil {
+		stdout = os.Stdout
+	}
+
+	if stderr == nil {
+		stderr = os.Stderr
+	}
+
+	l := Logger{
+		il: log.New(stdout, "", 0),
+		el: log.New(stderr, "", 0),
+	}
+
+	if debug {
+		l.dl = log.New(stdout, "", 0)
+	}
+
+	return &l
 }
