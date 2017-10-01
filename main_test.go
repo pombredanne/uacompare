1170a36f9899db1fa3547c9a7d32604e3bac1e5e
Davor Kapsa <davor.kapsa@gmail.com>
2017-10-01 14:00:17 +0200

    clean TestRun

--- main_test.go
+++ main_test.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
-	if got := run(testDataPath, false, nil, nil); got != exitOK {
+	var (
+		bo = &bytes.Buffer{}
+		be = &bytes.Buffer{}
+	)
+
+	if got := run(testDataPath, false, bo, be); got != exitOK {
 		t.Errorf("run(%q, ...) got %v, want %v", testDataPath, got, exitOK)
 	}
 }
