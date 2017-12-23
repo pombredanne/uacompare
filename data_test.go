66b9abf6a8e16e2882d8dd65dd6a87b08c1c8c18
Davor Kapsa <davor.kapsa@gmail.com>
2017-12-23 11:01:01 +0100

    add TestLoadDataInvalid

--- data_test.go
+++ data_test.go
@@ -5,12 +5,21 @@ import "testing"
 const testDataPath = "testdata/testdata.txt"
 
 func TestLoadData(t *testing.T) {
-	var d Data
-	err := loadData(new(Logger), testDataPath, &d)
+	d, err := loadData(new(Logger), testDataPath)
 	if err != nil {
 		t.Errorf("err: %v", err)
 	}
 	if len(d) == 0 {
 		t.Error("empty data")
+	}
+}
+
+func TestLoadDataInvalid(t *testing.T) {
+	d, err := loadData(new(Logger), "invalid")
+	if err == nil {
+		t.Error("nil error for invalid path")
+	}
+	if len(d) != 0 {
+		t.Error("data must be empty")
 	}
 }
